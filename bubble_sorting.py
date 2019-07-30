9a08f66316abfb2d354ef284022e7b03808c19c6
nutristar <deniszmail@gmail.com>
2019-07-30 05:46:00 +0300

    +111

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -1,25 +1,16 @@
-from tkinter import Tk,Entry,Label
-from pyautogui import click, moveTo
-from time import sleep
-def callback(event):
-	global k,entry
-	if entry.get()=="hello": k=True
-def on_closing():
-	click(675, 420)
-	moveTo(675, 420)
-	root.attributes("-fullscreen",True)
-	root.protocol("WM_DELETE_WINDOW", on_closing)
-	root.update()
-	root.bind('<Control-KeyPress-c>', callback)
-root=Tk() # Создание окна
-root.title("Locker") # Заголовочное название
-root.attributes("-fullscreen",True) # Расширение под экран
-entry=Entry(root,font=1) # Поле ввода
-entry.place(width=150,height=50,x=600,y=400) # Координаты и размеры
-label0=Label(root,text="Locker_by_#571",font=1) # Надпись 1
-label0.grid(row=0,column=0) # Координаты надписи 1
-label1=Label(root,text="Write the Password and Press Ctrl+C",font='Arial 20') # Надпись 2
-label1.place(x=470,y=300) # Координаты надписи 2
-root.update(); sleep(0.2); click(675, 420) # Обновление экрана программы 
-k=False
-while k!=True: on_closing()
+from random import randint
+n=10
+a=[]
+#print(b)
+for x in range(n):
+	a.append(randint(1,99))
+print(a)
+
+for i in range(n-1):
+	print("это i",i)
+	for j in range(n-i-1):
+		print(j)
+		if a[j]>a[j+1]:
+			a[j],a[j+1]= a[j+1],a[j]
+		print(a)
+print(a)
